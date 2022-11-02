eaed6e4fc17b7a4fa04d3f3009dbc725954fa7a5
kumail-r <46721070+kumail-r@users.noreply.github.com>
2022-11-02 02:30:00 -0400

    started work on generating step2

--- Assignment 3/generator.c
+++ Assignment 3/generator.c
@@ -1,13 +1,57 @@
+// By Kumail Raza
+// 105225432
+// 11/2/2022
+
 #include <stdio.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+void createStep2File(char wordList[100][100], int wordCount){
+    FILE* fp = fopen("step2.pl", "w"); // open file pointer
+    if(!fp) { // check if file loaded correctly
+        printf("Error in opening step2.pl");
+        exit(1); // if not, exit
+    }
+    fprintf(fp, "%% By Kumail Raza %%\n%% 105225432 %%\n\n");
+    fprintf(fp, "%% WORDS %%\n"); // print comment in file
+    for(int i = 0; i < wordCount; i++) { // iterate through all words
+        // convert word to lower case in order to work properly in prolog
+        char lower[strlen(wordList[i])]; // initialize lower case string
+        strcpy(lower, wordList[i]); // copy word to lower case variable
+        for(int k = 0; k < strlen(lower); k++) { 
+            if ((lower[k] >= 'A') && (lower[k] <= 'Z')) {
+                lower[k] = lower[k] + 32; // if upper case, convert to lower
+            }
+        }
+        // print out word facts (example: word(word, [w,o,r,d], word_length)))
+        fprintf(fp, "word(%s, [", lower); 
+        for(int j = 0; j < strlen(lower); j++) {
+            fprintf(fp, "%c", lower[j]);
+            if (j+1 < strlen(lower)){
+                fprintf(fp, ",");
+            }
+            else{
+                fprintf(fp, "], %ld).\n", strlen(wordList[i]));
+            }
+        }
+    }
+    // print out "intersection"
+    fprintf(fp, "\n%% WORD INTERSECTIONS %%\nintersection(Word1, Word2, Index1, Index2):-\n    dif(Word1, Word2),\n    word(Word1, List1, Length1),\n    nth0(Index1, List1, Letter),\n    Index1 < Length1,\n    word(Word2, List2, Length2),\n    nth0(Index2, List2, Letter),\n    Index2 < Length2.\n");
+    fprintf(fp, "\n%% CROSSWORD DATA %%\n");
+    fprintf(fp, "crossward(W1,W2,W3,W4,W5,W6,W7,W8,W9,W10,W11):-\n");
+    
+    // handle intersections
+    fprintf(fp, "    intersections(insert intersections code here!)\n");
+    
+    fclose(fp);
+}
+
 int main() {
     FILE * fp;
     fp = fopen("words.txt", "r");
     if(!fp) {
-        printf("Error in opening the file..!!");
+        printf("Error in opening words.txt");
         exit(1);
     }
     char ch = getc(fp);
@@ -57,12 +101,26 @@ int main() {
         }
         ch = getc(fp);
     }
-    for (int i = 0; i < cwRow + 1; i++){
-        for (int j = 0; j < cwCol; j++){
-            printf("%d", crosswardMatrix[i][j]);
-        }
-        printf("\n");
-    }
-    
+    createStep2File(wordList, wordCount);
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
